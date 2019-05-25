01c0d8e68eb34b7076424706eb3b8bf2b11b7ef7
Yasuhiro Fujii <y-fujii@mimosa-pudica.net>
2019-05-25 23:20:00 +0900

    init.

---
+++ plot.py
@@ -0,0 +1,36 @@
+from numpy import *
+from matplotlib import pyplot
+
+
+def plot(xs, tss, fn):
+    pyplot.figure(figsize = (8.0, 4.0))
+    pyplot.ylim(0.0, max(max(ts) for ts in tss))
+    pyplot.xlim(min(xs), max(xs))
+    for i, ts in enumerate(tss):
+        pyplot.plot(xs, ts, ["--", "-.", ":"][i % 3], label = "%d" % i, linewidth = 1.0)
+    pyplot.legend()
+    pyplot.savefig(fn, format = "svg", bbox_inches = "tight", pad_inches = 0.0)
+
+def plot_variance_normalized():
+    xs = linspace(-6.0, 3.0, 256)
+    gs = (1.0 / sqrt(2.0 * pi)) * exp(-0.5 * xs * xs)
+    ss = 0.5 / cosh((pi / 2.0) * xs)
+    a = pi / (2.0 * sqrt(3.0))
+    fs = (a / 2.0) / cosh(a * xs) ** 2
+    a = sqrt((pi * pi - 6.0) / 3.0)
+    hs = where(abs(xs) < pi / a, (a / (2.0 * pi)) * (1.0 + cos(a * xs)), 0.0)
+    #ds = 2.0 / (1.0 + exp(xs * xs))
+    plot(xs, [ss, fs, gs, hs], "variance.svg")
+
+def plot_normalized_at_zero():
+    xs = linspace(-8.0, 4.0, 256)
+    gs = exp(-0.5 * xs * xs)
+    ss = 1.0 / cosh(xs)
+    fs = 1.0 / cosh(xs / sqrt(2)) ** 2
+    hs = where(abs(xs) < 2.0, 0.5 + 0.5 * cos((pi / 2.0) * xs), 0.0)
+    cs = 1.0 / (1.0 + 0.5 * xs * xs)
+    #ds = 2.0 / (1.0 + exp(xs * xs))
+    plot(xs, [cs, ss, fs, gs, hs], "at_zero.svg")
+
+plot_variance_normalized()
+plot_normalized_at_zero()
