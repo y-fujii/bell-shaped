1c49b865212e7c0d635a2fc9d3cd748bb78e9ccb
Yasuhiro Fujii <y-fujii@mimosa-pudica.net>
2019-05-26 02:00:41 +0900

    add nu = 3 t-dist.

--- plot.py
+++ plot.py
@@ -4,13 +4,13 @@ from matplotlib import pyplot
 
 
 def plot(xs, tss, fn):
-    pyplot.figure(figsize = (8.0, 4.0))
-    pyplot.ylim(0.0, max(max(ts) for ts in tss))
+    pyplot.figure(figsize = (9.0, 6.0))
     pyplot.xlim(min(xs), max(xs))
+    pyplot.ylim(0.0, max(max(ts) for ts in tss))
     for i, ts in enumerate(tss):
         pyplot.plot(xs, ts, ["--", "-.", ":"][i % 3], label = "%d" % i, linewidth = 1.0)
     pyplot.legend()
-    pyplot.savefig(fn, format = "svg", bbox_inches = "tight", pad_inches = 0.0)
+    pyplot.savefig(fn, format = "svg", bbox_inches = "tight")
 
 def plot_variance_normalized():
     xs = linspace(-6.0, 3.0, 256)
@@ -20,8 +20,8 @@ def plot_variance_normalized():
     fs = (a / 2.0) / cosh(a * xs) ** 2
     a = sqrt((pi * pi - 6.0) / 3.0)
     hs = where(abs(xs) < pi / a, (a / (2.0 * pi)) * (1.0 + cos(a * xs)), 0.0)
-    #ds = 2.0 / (1.0 + exp(xs * xs))
-    plot(xs, [ss, fs, gs, hs], "variance.svg")
+    ts = (2.0 / pi) / (1.0 + xs * xs) ** 2
+    plot(xs, [ts, ss, fs, gs, hs], "variance.svg")
 
 def plot_normalized_at_zero():
     xs = linspace(-8.0, 4.0, 256)
@@ -30,7 +30,6 @@ def plot_normalized_at_zero():
     fs = 1.0 / cosh(xs / sqrt(2)) ** 2
     hs = where(abs(xs) < 2.0, 0.5 + 0.5 * cos((pi / 2.0) * xs), 0.0)
     cs = 1.0 / (1.0 + 0.5 * xs * xs)
-    #ds = 2.0 / (1.0 + exp(xs * xs))
     plot(xs, [cs, ss, fs, gs, hs], "at_zero.svg")
 
 plot_variance_normalized()
